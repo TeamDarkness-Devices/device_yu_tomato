a61b96702c4347c60cc88661a8028bfadff2903a
DarkKnight6499 <yazad.madan@gmail.com>
2017-07-20 04:45:33 +0000

    tomato: USE AudioFx
    Signed-off-by: DarkKnight6499 <yazad.madan@gmail.com>

--- aos.mk
+++ aos.mk
@@ -18,6 +18,9 @@ $(call inherit-product, device/yu/tomato/full_tomato.mk)
 # Magisk
 WITH_ROOT=true
 
+# AudioFX
+WITH_AUDIOFX=true
+
 # Inherit some common Atomic-OS stuff.
 $(call inherit-product, vendor/aos/config/common_full_phone.mk)
 
